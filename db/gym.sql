1c8a865539646b84323334b4a865e4f5fb0674ab
Elisa Bjorg Sveinsdottir <elisabjorg22@gmail.com>
2019-02-09 19:23:47 +0000

    All folders and files created. Database created and tables dropped

---
+++ db/gym.sql
@@ -0,0 +1,23 @@
+DROP TABLE bookings;
+DROP TABLE lessons;
+DROP TABLE members;
+
+CREATE TABLE lessons(
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255),
+  instructor VARCHAR(255)
+);
+
+CREATE TABLE members(
+  id SERIAL8 PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  address VARCHAR(255),
+  email VARCHAR(255)
+);
+
+CREATE TABLE bookings(
+  id SERIAL8 PRIMARY KEY,
+  lesson_id INT8 references lessons(id),
+  member_id INT8 references members(id)
+);
