c6c698b6c63be1547e1849d9225283f4a772bbe0
Elisa Bjorg Sveinsdottir <elisabjorg22@gmail.com>
2019-02-10 20:35:19 +0000

    def self.find(id) method added

--- models/member.rb
+++ models/member.rb
@@ -36,6 +36,14 @@ class Member
     return results.map { |member| Member.new( member ) }
   end
 
+  def self.find( id )
+    sql = "SELECT * FROM members
+    WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run( sql, values )
+    return Member.new( results.first )
+  end
+
   def self.delete_all()
     sql = "DELETE FROM members"
     SqlRunner.run( sql )
