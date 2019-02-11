b61d98e419142ae682fcdef2640fc78c37386d5e
Elisa Bjorg Sveinsdottir <elisabjorg22@gmail.com>
2019-02-11 13:27:48 +0000

    added get lessons id method

--- controllers/lessons_controller.rb
+++ controllers/lessons_controller.rb
@@ -9,12 +9,12 @@ get '/lessons' do
   @lessons = Lesson.all()
   erb (:"lessons/index")
 end
-# 
-# get '/lessons/:id' do
-#   @lesson = Lesson.find(params['id'].to_i)
-#   erb (:"lessons/show")
-# end
-#
+
+get '/lessons/:id' do
+  @lesson = Lesson.find(params['id'].to_i)
+  erb (:"lessons/show")
+end
+
 # get '/lesson/new' do
 #   erb (:"lessons/new")
 # end
