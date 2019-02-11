0131dd6e915b736d5bcb77e064185f1453141174
Elisa Bjorg Sveinsdottir <elisabjorg22@gmail.com>
2019-02-11 13:27:29 +0000

    added get lessons method

---
+++ controllers/lessons_controller.rb
@@ -0,0 +1,20 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative( '../models/lesson.rb' )
+
+also_reload( '../models/*' )
+
+get '/lessons' do
+  @lessons = Lesson.all()
+  erb (:"lessons/index")
+end
+# 
+# get '/lessons/:id' do
+#   @lesson = Lesson.find(params['id'].to_i)
+#   erb (:"lessons/show")
+# end
+#
+# get '/lesson/new' do
+#   erb (:"lessons/new")
+# end
