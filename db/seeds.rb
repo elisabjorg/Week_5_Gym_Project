20274b5d78de15669511a19085ee005acd0beeb4
Elisa Bjorg Sveinsdottir <elisabjorg22@gmail.com>
2019-02-11 23:29:44 +0000

    added new member, lesson and booking

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,6 +34,15 @@ Lesson.delete_all()
 
   member_3.save()
 
+  member_4 = Member.new({
+    "first_name" => "Jennifer",
+    "last_name" => "Aniston",
+    "address" => "Princess Garden",
+    "email" => "jennifer@gmail.com"
+  })
+
+  member_4.save()
+
   lesson_1 = Lesson.new({
     "title" => "Fitness Box",
     "instructor" => "Lauren Hill"
@@ -55,6 +64,13 @@ Lesson.delete_all()
 
   lesson_3.save()
 
+  lesson_4 = Lesson.new({
+    "title" => "Hot Yoga",
+    "instructor" => "Jay Leno"
+  })
+
+  lesson_4.save()
+
   booking_1 = Booking.new({
     "member_id" => member_1.id,
     "lesson_id" => lesson_1.id
@@ -75,6 +91,15 @@ Lesson.delete_all()
     })
 
   booking_3.save()
+
+  booking_4 = Booking.new({
+    "member_id" => member_2.id,
+    "lesson_id" => lesson_1.id
+    })
+
+  booking_4.save()
+
+
 
 binding.pry
 nil
