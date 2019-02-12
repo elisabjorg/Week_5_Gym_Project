74ef551dc7322ddb5f8a6f0890023f73187ae159
Elisa Bjorg Sveinsdottir <elisabjorg22@gmail.com>
2019-02-12 14:28:49 +0000

    delete method added to lesson.rb

--- models/lesson.rb
+++ models/lesson.rb
@@ -45,6 +45,12 @@ class Lesson
     SqlRunner.run(sql, values)
   end
 
+  def delete()
+    sql = 'DELETE FROM lessons WHERE id = $1'
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
 
   def self.all()
     sql = "SELECT * FROM lessons"
